dd554fce37fe8113cd132f4c985108131d1969a2
sakumae1 <sakumae@msoe.edu>
2026-09-11 09:06:03 -0500

    Updated for use w/photoresistor

--- Week 02 Lab - Analog Input/src/main.cpp
+++ Week 02 Lab - Analog Input/src/main.cpp
@@ -6,6 +6,7 @@ int myFunction(int, int);
 void setup() {
   Serial.begin(9600);
   delay(1000);
+
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
   pinMode(0, INPUT_PULLUP);
@@ -14,12 +15,22 @@ void setup() {
   
 }
 
+long prevSampleTime = 0;
+long timeBetweenSamplesMs = 100;
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("D0, D1, D2:");
-  Serial.print(digitalRead(0));
-  Serial.print(digitalRead(1));
-  Serial.println(digitalRead(2));
+  long currentTime = millis();
+  if (millis() > prevSampleTime + timeBetweenSamplesMs) {    
+    // put your main code here, to run repeatedly:
+    Serial.print("D0, D1, D2, A0: ");
+    Serial.print(digitalRead(0));
+    Serial.print(digitalRead(1));
+    Serial.print(digitalRead(2));
+    Serial.print("    ");
+    Serial.println(analogRead(A0));
+
+        // delayMicroseconds(100); // delay so that we can sample data and/or execute some task
+        // NOTE: Serial.print an delayMicroseconds prevent everything else from happening when they are executed
+    prevSampleTime = currentTime; }
 }
 
 // put function definitions here:
